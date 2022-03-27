7a15c1a66adc6d937314015dfec7a253dbb9fed2
Mowo <toryvick20@gmail.com>
2022-03-27 23:46:09 +0100

    Update find-char_in_str.py

--- find-char_in_str.py
+++ find-char_in_str.py
@@ -11,4 +11,29 @@ def char_in_str():
     return total
     #else:
      #   print("This character is not in this word")
-char_in_str()
+
+#While Loop
+def char_in_str_2():
+    string = input("Enter word here: ")
+    target = input("Enter character to look for here: ") 
+    total = 0
+    index = 0
+    while index < len(string):
+        if index == target:
+            total += 1
+         index += 1
+    return total
+
+#Select loop
+def loop_choice():
+    selection = input("Which loop would you prefer to find your character in your string? while/for? \n").lower()
+    if selection == "for":
+        print(char_in_str())
+    elif selection == "while":
+        print(char_in_str_2())
+    else:
+        print("Invalid loop selection")
+        loop_choice()
+        
+loop_choice()
+        
